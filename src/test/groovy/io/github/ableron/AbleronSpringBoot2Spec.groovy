6d2fc12322c04431bda5522d7c9e915d6f67a858
Torsten <tbcollect.ableron@web.de>
2023-02-18 22:32:32 +0100

    separate test runs per lib

---
+++ src/test/groovy/io/github/ableron/AbleronSpringBoot2Spec.groovy
@@ -0,0 +1,16 @@
+package io.github.ableron
+
+import org.testcontainers.containers.GenericContainer
+import org.testcontainers.images.builder.ImageFromDockerfile
+
+import java.nio.file.Path
+
+class AbleronSpringBoot2Spec extends UiCompositionBaseSpec {
+
+  @Override
+  GenericContainer getContainerUnderTest() {
+    return new GenericContainer<>(new ImageFromDockerfile()
+      .withDockerfile(Path.of("ableron-spring-boot-2", "Dockerfile")))
+      .withExposedPorts(8080)
+  }
+}
