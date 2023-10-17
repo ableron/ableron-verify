5d3fc626bfaa8d194610e44647645a50307073bf
Torsten <dev@t-blasche.de>
2023-10-18 00:14:23 +0200

    make ableron-js tests work

--- ableron-js-nodejs20/src/app.js
+++ ableron-js-nodejs20/src/app.js
@@ -1,18 +1,22 @@
 const express = require('express')
-const { Ableron } = require('ableron')
 const bodyParser = require('body-parser')
 const app = express()
 const port = 8080
+const { Ableron, AbleronConfig } = require('ableron');
 
-const ableron = new Ableron('ableron test')
+const ableron = new Ableron(new AbleronConfig())
 
-app.use(bodyParser.text({ type: 'text/*' }))
+app.use(bodyParser.text({ type: 'text/*', limit: '5MB' }))
 
-app.post('/verify', (req, res) => {
-  res.setHeader('Content-Type', 'text/html; charset=utf-8')
-  res.send(req.body)
+app.post('/verify', async (req, res) => {
+  const transclusionResult = await ableron.resolveIncludes(req.body, req.headers)
+  res
+    .setHeader('Content-Type', 'text/html; charset=utf-8')
+    .setHeader('Cache-Control', transclusionResult.calculateCacheControlHeaderValueByResponseHeaders(res.getHeaders()))
+    .status(transclusionResult.getStatusCodeOverride() || 200)
+    .send(transclusionResult.getContent())
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Listening on port ${port}`)
 })
